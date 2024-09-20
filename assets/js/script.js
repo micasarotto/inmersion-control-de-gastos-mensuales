698a7f99382cf0ede81073555ae9d74f00e53dc3
Micaela Sarotto <micaelasarotto@gmail.com>
2024-09-20 16:09:33 -0300

    funcionalidad

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+let listaNombresGastos = [];
+let listaValoresGastos = [];
+
+function clickBoton() {
+    let nombreGasto = document.getElementById('nombreGasto').value;
+    let valorGasto = document.getElementById('valorGasto').value;
+
+    listaNombresGastos.push(nombreGasto);
+    listaValoresGastos.push(valorGasto);
+
+    actualizarListaGastos();
+}
+
+function actualizarListaGastos() {
+    const listaElementos = document.getElementById('listaDeGastos');
+    const totalElementos = document.getElementById('totalGastos');
+    let htmlLista = '';
+    let totalGastos = 0;
+    listaNombresGastos.forEach((elemento, posicion) => {
+        const valorGasto = Number(listaValoresGastos[posicion]);
+
+        htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)} <button onclick="eliminarGasto(${posicion});">Eliminar</button></li>`;
+
+        totalGastos += Number(valorGasto);
+    });
+    listaElementos.innerHTML = htmlLista;
+    totalElementos.innerHTML = totalGastos.toFixed(2);
+    limpiar();
+}
+
+function limpiar() {
+    document.getElementById('nombreGasto').value = '';
+    document.getElementById('valorGasto').value = '';
+}
+function eliminarGasto(posicion){
+    listaNombresGastos.splice(posicion,1);
+    listaValoresGastos.splice(posicion,1);
+    actualizarListaGastos();
+}
